aee4a4decf6c2e2e3b80151e4a0b250fd354b5c4
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-04-03 16:34:15 +0545

    chore: animated shopping card button

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
 
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isExpaned = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,16 +16,47 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpaned = !isExpaned;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            width: isExpaned ? 200.0 : 120.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isExpaned ? Colors.orange : Colors.blue,
+              borderRadius: isExpaned
+                  ? BorderRadius.circular(100.0)
+                  : BorderRadius.circular(10.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Visibility(
+                  visible: isExpaned,
+                  child: const Icon(
+                    Icons.check,
+                    color: Colors.white,
+                  ),
+                ),
+                isExpaned
+                    ? const FittedBox(
+                        child: Text(
+                        "Add to cart",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                        ),
+                      ))
+                    : const Icon(
+                        Icons.shopping_cart,
+                        color: Colors.white,
+                      ),
+              ],
+            ),
           ),
         ),
       ),
